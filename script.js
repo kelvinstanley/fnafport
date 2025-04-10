f94a57876ff9be4f05a939eaacdb6247d3bdcebd
kel <146475381+kelvinstanley@users.noreply.github.com>
2025-04-10 13:32:40 -0300

    a

---
+++ script.js
@@ -0,0 +1,39 @@
+function clickImagem() {
+    const texto = document.getElementById('para1') 
+
+    if(texto.classList.contains('mostrar')) {
+        texto.classList.remove('mostrar')
+    } else {
+        texto.classList.add('mostrar')
+    }
+}
+
+function clickImagem2() {
+    const texto2 = document.getElementById('para2') 
+
+    if(texto2.classList.contains('mostrar2')) {
+        texto2.classList.remove('mostrar2')
+    } else {
+        texto2.classList.add('mostrar2')
+    }
+}
+
+function clickImagem3() {
+    const texto3 = document.getElementById('para3') 
+
+    if(texto3.classList.contains('mostrar3')) {
+        texto3.classList.remove('mostrar3')
+    } else {
+        texto3.classList.add('mostrar3')
+    }
+}
+
+function clickImagem4() {
+    const texto4 = document.getElementById('para4') 
+
+    if(texto4.classList.contains('mostrar4')) {
+        texto4.classList.remove('mostrar4')
+    } else {
+        texto4.classList.add('mostrar4')
+    }
+}
